43fadd788efcf0e6dfc1566131a3ea9455dc5a3f
kdkdhoho <hkim4410@naver.com>
2022-11-24 12:18:16 +0900

    feat(Quiz1): quiz2 추가

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Quiz1 {
 
@@ -16,23 +17,25 @@ public class Quiz1 {
         Map<String, Integer> result = new HashMap<>();
 
         csvLines.stream().map(line -> line[1].trim())
-                .peek(System.out::println)
                 .map(line -> line.split(":"))
                 .flatMap(Arrays::stream)
-                .peek(System.out::println)
                 .forEach(hobby -> result.put(hobby, result.getOrDefault(hobby, 0) + 1));
-
         return result;
     }
 
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+        Map<String, Integer> result = new HashMap<>();
+
+        csvLines.stream().filter(line -> line[0].startsWith("정"))
+                .map(line -> line[1].trim().split(":"))
+                .flatMap(Arrays::stream)
+                .forEach(hobby -> result.put(hobby, result.getOrDefault(hobby, 0) + 1));
+        return result;
     }
 
     public int quiz3() throws IOException {
-        List<String[]> csvLines = readCsvLines();
-        return 0;
+
     }
 
     private List<String[]> readCsvLines() throws IOException {
