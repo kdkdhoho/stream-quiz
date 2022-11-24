890241c692e60d0f441ca2f4af8dc61792ce6b88
kdkdhoho <hkim4410@naver.com>
2022-11-24 13:57:44 +0900

    feat(Quiz2): 완료

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -10,11 +10,42 @@ public class Quiz2 {
     private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
 
     public Map<String, Integer> quiz1() {
-        return new HashMap<>();
+        Map<String, Integer> result = new HashMap<>();
+
+        // for
+//        for (String word : WORDS) {
+//            String substring = word.substring(0, 1);
+//            result.put(substring, result.getOrDefault(substring, 0) + 1);
+//        }
+
+        // stream
+        WORDS.stream()
+                .forEach(word -> {
+                    String substring = word.substring(0, 1);
+                    result.put(substring, result.getOrDefault(substring, 0) + 1);
+                });
+        return result;
     }
 
     public String quiz2() {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // for
+//        for (String word : WORDS) {
+//            if (word.length() >= 2) {
+//                stringBuilder.append(word.substring(0, 1).toUpperCase() + " ");
+//            }
+//        }
+
+        // tream
+        WORDS.stream()
+                .filter(word -> word.length() >= 2)
+                .map(word -> word.toUpperCase())
+                .forEach(word -> {
+                    String substring = word.substring(0, 1);
+                    stringBuilder.append(substring + " ");
+                });
+        return stringBuilder.toString().trim();
     }
 
 }
